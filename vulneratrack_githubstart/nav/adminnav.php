1976bde9f6950733974c0044a26b4fd19cff1682
KurtttS <satumbagakurtallen@gmail.com>
2026-02-25 14:18:57 +0800

    Delete vulneratrack_githubstart directory

--- vulneratrack_githubstart/nav/adminnav.php
+++
@@ -1,5 +0,0 @@
-<header class="homeheader-nav">
-    <div class="homeheader-logo">
-        <img src="<?php echo $path_to_root ?? ''; ?>style/imgs/headerlogo.png" alt="Logo">    
-    </div>
-</header>
